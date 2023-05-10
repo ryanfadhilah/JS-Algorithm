36f6455daea166bb44f703c07c831c15fffae4dc
ryanfadhilah <ryan.fadhilah.acc@gmail.com>
2023-05-10 19:16:45 +0700

    lesson 7

---
+++ 7.fibonacci.js
@@ -0,0 +1,18 @@
+function fibonacci(n) {
+    const fib = [0, 1]
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2]
+    }
+    return fib
+}
+
+// 3 [0,1,1]
+// 4 [0,1,1,2]
+// 5 [0,1,1,2,3]
+// 6 [0,1,1,2,3,5]
+
+console.log(fibonacci(2)) // [0,1]
+console.log(fibonacci(3)) // [0,1,1]
+console.log(fibonacci(7)) // [0,1,1,2,3,5,8]
+
+// big - O = O(n)
