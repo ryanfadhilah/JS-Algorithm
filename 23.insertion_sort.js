a68a6fe0e2a303e7dfab29f06de59c9f95084cb6
ryanfadhilah <ryan.fadhilah.acc@gmail.com>
2023-05-29 08:58:20 +0700

    done

--- 23.insertion_sort.js
+++ 23.insertion_sort.js
@@ -1,21 +1,25 @@
 
 
 function insertion(arr) {
-
-    let done
-    let not_done
-    let memory
-
-    for (let i = 0; i < arr.length; i++) {
-        // check As dengan angka di depan
-        if (arr[i] > arr[i + 1]) {
-            memory = ar[i + 1]
-            // ar[i + 1] = ar[i]
-
+    for (let i = 1; i < arr.length; i++) {
+        let sorted = i - 1
+        let unsorted = arr[i]
+        while (arr[sorted] > unsorted && sorted >= 0) {
+            arr[sorted + 1] = arr[sorted]
+            sorted = sorted - 1
         }
+        arr[sorted + 1] = unsorted
     }
 }
-
-const arr = [-6, 20, 8, -2, 4]
+const arr = [-6, 7, -8, 2, 4, -5, -1, 9]
 insertion(arr)
 console.log(arr)
+    //     for (let i = 1; i < arr.length; i++) {
+    //         let unsorted = arr[i]
+    //         let sorted = i - 1
+    //         while (sorted >= 0 && arr[sorted] > unsorted) {
+    //             arr[sorted + 1] = arr[sorted]
+    //             sorted = sorted - 1
+    //         }
+    //         arr[sorted + 1] = unsorted
+    //     }
